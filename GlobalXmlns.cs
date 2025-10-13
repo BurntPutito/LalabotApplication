3a9cba23d4c32c1a0b99ea6581b7c244b7151631
BurntPutito <markcaitan18@gmail.com>
2025-10-13 19:32:39 +0800

    Initial commit

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LalabotApplication")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LalabotApplication.Pages")]
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LalabotApplication
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
