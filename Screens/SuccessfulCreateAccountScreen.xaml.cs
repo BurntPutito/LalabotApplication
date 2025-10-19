d8741c48fcfefbcfe45e977ddd087ff3641614af
BurntPutito <markcaitan18@gmail.com>
2025-10-19 18:19:10 +0800

    Added 5 seconds timer after showing CreateAccountSuccessful screen

--- Screens/SuccessfulCreateAccountScreen.xaml.cs
+++ Screens/SuccessfulCreateAccountScreen.xaml.cs
@@ -4,15 +4,50 @@ namespace LalabotApplication.Screens;
 
 public partial class SuccessfulCreateAccountScreen : ContentPage
 {
+    private int _secondsRemaining = 5;
+    private bool _hasNavigated = false;
+
 	public SuccessfulCreateAccountScreen()
 	{
 		InitializeComponent();
 	}
 
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        StartCountdown();
+    }
+
+    private async void StartCountdown()
+    {
+        _secondsRemaining = 5;
+        _hasNavigated = false;
+
+        while (_secondsRemaining > 0 && !_hasNavigated)
+        {
+            TimerLabel.Text = $"Redirecting to login in {_secondsRemaining} second{(_secondsRemaining != 1 ? "s" : "")}...";
+            await Task.Delay(1000);
+            _secondsRemaining--;
+        }
+
+        if (!_hasNavigated)
+        {
+            await NavigateToLogin();
+        }
+    }
+
+    private async Task NavigateToLogin()
+    {
+        if (_hasNavigated) return;
+
+        _hasNavigated = true;
+        await Shell.Current.GoToAsync("///Login");
+    }
     
 
     private async void NavigateLogin_Clicked(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync("///Login");
+        await NavigateToLogin();
     }
+
 }
