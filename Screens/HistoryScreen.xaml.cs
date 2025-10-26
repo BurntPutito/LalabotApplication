5ece9b3bd91e67f1b9fd8403e5a5de1303f3389d
BurntPutito <markcaitan18@gmail.com>
2025-10-26 18:49:41 +0800

    Added filter button in HistoryScreen

--- Screens/HistoryScreen.xaml.cs
+++ Screens/HistoryScreen.xaml.cs
@@ -6,4 +6,23 @@ public partial class HistoryScreen : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        // Reset all buttons to default color
+        DeliveredBtn.BackgroundColor = Colors.AliceBlue;
+        DeliveredBtn.BorderColor = Color.FromRgba("#2D4A6E");
+        DeliveredBtn.TextColor = Colors.Black;
+        CancelledBtn.BackgroundColor = Colors.AliceBlue;
+        CancelledBtn.BorderColor = Color.FromRgba("#2D4A6E");
+        CancelledBtn.TextColor = Colors.Black;
+        PendingBtn.BackgroundColor = Colors.AliceBlue;
+        PendingBtn.BorderColor = Color.FromRgba("#2D4A6E");
+        PendingBtn.TextColor = Colors.Black;
+        // Highlight the clicked one
+        var clickedButton = (Button)sender;
+        clickedButton.TextColor = Colors.White;
+        clickedButton.BackgroundColor = Color.FromRgba("#2D4A6E");
+    }
+
 }
