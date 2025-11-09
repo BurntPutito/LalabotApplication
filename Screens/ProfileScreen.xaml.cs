f8612b86210ee74f28b9416c9877200c8c73e404
BurntPutito <markcaitan18@gmail.com>
2025-11-10 00:46:17 +0800

    Update ProfileScreen.xaml.cs

--- Screens/ProfileScreen.xaml.cs
+++ Screens/ProfileScreen.xaml.cs
@@ -1,9 +1,21 @@
-namespace LalabotApplication.Screens;
+﻿namespace LalabotApplication.Screens;
 
 public partial class ProfileScreen : ContentPage
 {
-	public ProfileScreen()
-	{
-		InitializeComponent();
-	}
+    public ProfileScreen(ProfileScreenModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        // Reload profile data when screen appears
+        if (BindingContext is ProfileScreenModel viewModel)
+        {
+            _ = viewModel.LoadProfileData();
+        }
+    }
 }
